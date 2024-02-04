0aeefc3546646d9cdb52acdd3d5d002ea1ccee05
Jon Koops <jonkoops@gmail.com>
2024-02-04 22:59:00 +0100

    Add custom HTML to load Leaflet styles

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,7 +1,21 @@
 import {importMapsPlugin} from '@web/dev-server-import-maps';
 
+/** @type {import('@web/test-runner').TestRunnerConfig} */
 export default {
 	nodeResolve: true,
+	testRunnerHtml: testFramework => `
+		<!doctype html>
+		<html lang="en">
+			<head>
+				<title>Leaflet Tests</title>
+				<meta charset="utf-8">
+				<link rel="stylesheet" href="/dist/leaflet.css">
+			</head>
+			<body>
+				<script type="module" src="${testFramework}"></script>
+			</body>
+		</html>
+	`,
 	plugins: [
 		importMapsPlugin({
 			inject: {
